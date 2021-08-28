98b4cebcb341d0fbdd680126bf9d5cb1c4223c77
HernandoNJ <hernandonj.unity@gmail.com>
2021-08-28 17:13:32 -0500

    Player movement and animations finished

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,10 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
     private PlayerAnimation playerAnim;
-    private SpriteRenderer spriteRend;
+    private SpriteRenderer playerSpriteRend;
+
+    [SerializeField] private GameObject swordArcGO;
+    [SerializeField] private SpriteRenderer swordArcRend;
     [SerializeField] private float jumpForce = 5f;
     [SerializeField] private float speed = 5f;
     [SerializeField] private bool resetJump;
@@ -16,47 +19,68 @@ public class Player : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         playerAnim = GetComponent<PlayerAnimation>();
-        spriteRend = GetComponentInChildren<SpriteRenderer>();
+        playerSpriteRend = GameObject.Find("PlayerSprite").GetComponent<SpriteRenderer>();
+        swordArcGO = GameObject.Find("SwordArc");
+        swordArcRend = swordArcGO.GetComponent<SpriteRenderer>();
+
+        if (rb == null || playerAnim == null || playerSpriteRend == null || swordArcGO == null || swordArcRend == null)
+            Debug.LogWarning("Set required components in player script");
     }
 
     private void Update()
     {
         MovePlayer();
+        CheckGrounded();
     }
 
-    private bool Grounded()
+    private void CheckGrounded()
     {
+        Debug.DrawRay(transform.position, Vector3.down * 0.8f, Color.green);
+
         var hitInfo = Physics2D.Raycast(origin: transform.position, direction: Vector2.down, distance: 0.8f,
                 layerMask: groundLayerMask);
 
         if (hitInfo.collider != null)
         {
-            if (resetJump == false)
-            {
-                playerAnim.SetJumpBool(false);
-                return true;
-            }
+            isGrounded = true;
+            if (resetJump == false) playerAnim.SetJumpAnim(false);
         }
-
-        return false;
+        else isGrounded = false;
     }
 
     private void MovePlayer()
     {
         var moveH = Input.GetAxisRaw("Horizontal") * speed;
-        spriteRend.flipX = moveH < 0;
-        
-        Grounded(); // for setJump to false
+        var flipped = moveH < 0;
+        FlipPlayer(flipped);
+       
 
-        if (Input.GetKeyDown(KeyCode.Space) && Grounded())
+        var spacePressed = Input.GetKeyDown(KeyCode.Space);
+        var leftBtnPressed = Input.GetMouseButtonDown(0);
+
+        if (spacePressed && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             StartCoroutine(ResetJumpRoutine());
-            playerAnim.SetJumpBool(true);
+            playerAnim.SetJumpAnim(true);
         }
-        
+
+        if (leftBtnPressed && isGrounded) playerAnim.SetAttackAnim();
+
         rb.velocity = new Vector2(moveH, rb.velocity.y);
-        playerAnim.SetSpeedFloat(moveH);
+        playerAnim.SetRunAnim(moveH);
+    }
+
+    private void FlipPlayer(bool isFlipped)
+    {
+        playerSpriteRend.flipX = isFlipped;
+        swordArcRend.flipX = isFlipped;
+        swordArcRend.flipY = isFlipped;
+        
+        var newPos = swordArcGO.transform.localPosition;
+        if (isFlipped) newPos.x = -1f;
+        else newPos.x = 1;
+        swordArcGO.transform.localPosition = newPos;
     }
 
     private IEnumerator ResetJumpRoutine()
--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -3,20 +3,29 @@ using UnityEngine;
 public class PlayerAnimation : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private Animator swordAnimator;
 
     private void Start()
     {
         animator = GetComponentInChildren<Animator>();
+        if(animator == null || swordAnimator == null) Debug.LogWarning("Place the animator");
     }
 
-    public void SetSpeedFloat(float speedArg)
+    public void SetRunAnim(float speedArg)
     {
         animator.SetFloat("speed", Mathf.Abs(speedArg));
     }
 
-    public void SetJumpBool(bool jumpArg)
+    public void SetJumpAnim(bool jumpArg)
     {
         animator.SetBool("jumping", jumpArg);
     }
+
+    public void SetAttackAnim()
+    {
+        animator.SetTrigger("attack");
+        swordAnimator.SetTrigger("swordAttack");
+    }
+    
 }
 
